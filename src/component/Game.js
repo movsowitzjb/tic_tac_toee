141393d7201690fd0736981f54310ac9cc54202f
movsowitzjb <movsowitzjb@gmail.com>
2021-08-26 21:39:39 +0200

    jj

---
+++ src/component/Game.js
@@ -0,0 +1,69 @@
+import { useState, useEffect } from "react";
+import Square from "./Square";
+import Restart from "./Restart";
+import calculateWinner from "./calculateWinner";
+import isBoardFull from "./isBoardFull";
+
+const Game = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isNext, setIsNext] = useState(true);
+  const nextSymbol = isNext ? "X" : "O";
+  const winner = calculateWinner(squares);
+
+  const renderSquare = (i) => {
+    const squareHandler = () => {
+      if (squares[i] != null || winner != null) {
+        return;
+      }
+      const nextSquares = squares.slice();
+      nextSquares[i] = nextSymbol;
+      setSquares(nextSquares);
+      setIsNext(!isNext);
+    };
+    return <Square value={squares[i]} onClick={squareHandler} />;
+  };
+
+  const renderRestartButton = () => {
+    const restartHandler = () => {
+      setSquares(Array(9).fill(null));
+      setIsNext(true);
+    };
+    return <Restart onClick={restartHandler} />;
+  };
+  const getStatus = () => {
+    if (winner) {
+      return "Winner:" + winner;
+    } else if (isBoardFull(squares)) {
+      return "Draw";
+    } else {
+      return "Next Player:" + nextSymbol;
+    }
+  };
+  return (
+    <div className="container">
+      <div className="game">
+        <div className="game-board">
+          <div className="board-row">
+            {renderSquare(0)}
+            {renderSquare(1)}
+            {renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {renderSquare(3)}
+            {renderSquare(4)}
+            {renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {renderSquare(6)}
+            {renderSquare(7)}
+            {renderSquare(8)}
+          </div>
+        </div>
+        <div className="game-info">{getStatus()}</div>
+        <div className="restart-button">{renderRestartButton()}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Game;
---
+++ src/component/Restart.js
@@ -0,0 +1,9 @@
+const Restart = ({ onClick }) => {
+  return (
+    <button className="restart" onClick={onClick}>
+      Play Again?
+    </button>
+  );
+};
+
+export default Restart;
